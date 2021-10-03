15d4c59d4ace3ecad20333937773981d88d3b414
jaida <jaidarya@gmail.com>
2021-10-03 16:04:20 -0400

    made m method

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -12,5 +12,10 @@ public class Third {
 
     public static void main(String[] args) {
         System.out.println("5");
+       // m();
+
+        public static void m(){
+            System.out.println(6);
+        }
     }
 }
